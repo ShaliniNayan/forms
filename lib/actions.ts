1e728b3c5120f471d77a37e53d1fbe144e16cf89
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-01-23 21:50:37 +0530

    Update response function

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { getSession } from './auth';
 import { prisma } from './prisma';
 import { response } from 'express';
+import { redirect } from 'next/navigation';
 
 export const createForm = async () => {
   const session = await getSession();
@@ -312,4 +313,134 @@ export const deleteQuestion = async (formId: string, questionId: string) => {
   revalidatePath(`forms/${formId}`);
 
   return;
+};
+
+export const togglePublishFormFromUser = async (formId: string) => {
+  const session = await getSession();
+  if (!session?.user.id) {
+    return {
+      error: 'Not authenticated',
+    };
+  }
+
+  const form = await prisma.form.findFirstOrThrow({
+    where: {
+      id: formId,
+      userId: session.user.id,
+    },
+  });
+
+  const response = await prisma.form.update({
+    where: {
+      id: formId,
+      userId: session.user.id,
+    },
+    data: {
+      published: !form.published,
+    },
+  });
+
+  revalidatePath(`/forms/${formId}`);
+  return response;
+};
+
+export const getForm = async (formId: string) => {
+  const response = await prisma.form.findFirst({
+    where: {
+      id: formId,
+    },
+  });
+
+  if (!response) {
+    redirect('/forms/e');
+  }
+
+  if (!response.published) {
+    redirect('/forms/e');
+  }
+
+  return response;
+};
+
+function transform(obj: any) {
+  const result = [];
+  for (let key in obj) {
+    result.push({
+      answerText: obj[key],
+      questionId: key,
+    });
+  }
+
+  return result;
+}
+
+export const submitForm = async (answerHash: string, formId: string) => {
+  const answers = transform(answerHash);
+
+  const form = await prisma.form.findFirstOrThrow({
+    where: {
+      id: formId,
+    },
+  });
+
+  answers.map(async (answer) => {
+    const question = await prisma.question.findFirstOrThrow({
+      where: {
+        id: answer.questionId,
+      },
+    });
+
+    if (question.formId !== formId) {
+      throw new Error();
+    }
+    return answer;
+  });
+
+  const response = await prisma.response.create({
+    data: {
+      submittedAt: new Date().toISOString(),
+    },
+  });
+
+  const createAnswerOperations = answers.map((answer) => {
+    return prisma.answer.create({
+      data: {
+        responseId: response.id,
+        questionId: answer.questionId,
+        answerText: answer.answerText,
+        formId: form.id,
+      },
+    });
+  });
+
+  await prisma.$transaction(createAnswerOperations);
+
+  return;
+};
+
+export const getResponsesSummaryFromUser = async (formId: string) => {
+  const session = await getSession();
+  if (!session?.user.id) {
+    return {
+      error: 'Not authenticated',
+    };
+  }
+
+  const questions = await prisma.question.findMany({
+    where: {
+      formId: formId,
+    },
+    include: {
+      answers: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  });
+
+  return questions;
 };
