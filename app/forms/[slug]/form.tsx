b8b0a12243b4125b4397e2df17f965a933731d4b
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-01-29 11:18:03 +0530

    Update the page for register and dashboard

--- app/forms/[slug]/form.tsx
+++ app/forms/[slug]/form.tsx
@@ -20,7 +20,7 @@ import {
   updateQuestionFromUser,
 } from '@/lib/actions';
 import { useDebouncedCallback } from 'use-debounce';
-import { Plus, Trash2 } from 'lucide-react';
+import { MoveLeft, Plus, Trash2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
@@ -89,121 +89,137 @@ const QuestionForm = ({
   );
 
   return (
-    <div className='mx-48 my-24'>
-      <div className='my-10'>
-        <Link href={`/forms`}>{'<-- Back to my forms'}</Link>
-      </div>
-      <Input
-        defaultValue={title}
-        placeholder='Type form title'
-        className='border-0 shadow-none focus-visible:ring-0 pl-0 !mt-0 !pt-0 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'
-        onChange={(e) => formTitleDebounced(formId, e.target.value)}
-      />
-      <div className='mt-4'>
-        <Button
-          type='button'
-          variant='outline'
-          size='sm'
-          className='mt-2'
-          onClick={async () => {
-            await createQuestion(formId, questions.length);
-          }}
-        >
-          Add question
-        </Button>
-        <Button
-          type='button'
-          variant='outline'
-          size='sm'
-          className='mt-2 ml-8'
-          onClick={() => {
-            router.push(`/forms/preview/${formId}`);
-          }}
-        >
-          Preview
-        </Button>
-        <Button
-          type='button'
-          size='sm'
-          className='mt-2 ml-2'
-          onClick={async () => {
-            await togglePublishFormFromUser(formId);
-          }}
-        >
-          {form.published ? 'Unpublish' : 'Publish'}
-        </Button>
-        {form.published ? (
-          <div>
-            <Button
-              type='button'
-              size='sm'
-              className='mt-8'
-              onClick={async () => {
-                await navigator.clipboard.writeText(
-                  `${window.location.origin}/forms/viewform/${formId}`
-                );
-                toast({
-                  title: 'Link successfully copied',
-                });
-              }}
-            >
-              Copy Link
-            </Button>
-            <Button
-              type='button'
-              size='sm'
-              variant='outline'
-              className='mt-8 ml-2'
-              onClick={async () => {
-                router.push(`/forms/viewform/${formId}`);
-              }}
-            >
-              Go to form
-            </Button>
-          </div>
-        ) : null}
-      </div>
-      <div className='mt-12'>
-        {questions.map((element: any) => {
-          return (
-            <div key={element.id} className='mb-5 group relative'>
-              <Input
-                defaultValue={element.text}
-                key={element.id + '2'}
-                placeholder='Type a question'
-                className='w-1/2 border-0 shadow-none focus-visible:ring-0 pl-0 !mt-0 !pt-0 scroll-m-20 tracking-tight transition-colors leading-7 [&:not(:first-child)]:mt-0'
-                onChange={(e) => debounced(element.id, null, e.target.value)}
-              />
-              <Input
-                defaultValue={element.placeholder}
-                placeholder='Type a placeholder for the response'
-                key={element.id + '1'}
-                className=' w-1/2 leading-7 [&:not(:first-child)]:mt-0 text-muted-foreground'
-                onChange={(e) => debounced(element.id, e.target.value, null)}
-              />
-              <div className='absolute top-2 left-0 transform-translate-x-full hidden group-hover:inline-flex'>
-                <div className='mr-6'>
-                  <div className='px-2 hover:cursor-pointer'>
-                    <Plus
-                      className='text-gray-700'
+    <div className='mx-auto my-6 mt-16 sm:my-24 w-full max-w-xs sm:max-w-4xl'>
+      <div className=''>
+        <div className='my-10'>
+          <Link href={`/forms`}>
+            <div className='flex items-center'>
+              {
+                <MoveLeft
+                  className='mr-2'
+                  color='#000000'
+                  strokeWidth={1.75}
+                  absoluteStrokeWidth
+                  size={18}
+                />
+              }
+              {'Back to forms'}
+            </div>
+          </Link>
+        </div>
+
+        <Input
+          defaultValue={title}
+          placeholder='Type form title'
+          className='border-0 shadow-none focus-visible:ring-0 pl-0 !mt-0 !pt-0 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'
+          onChange={(e) => formTitleDebounced(formId, e.target.value)}
+        />
+        <div className='mt-4'>
+          <Button
+            type='button'
+            variant='outline'
+            size='sm'
+            className='mt-2'
+            onClick={async () => {
+              await createQuestion(formId, questions.length);
+            }}
+          >
+            Add question
+          </Button>
+          <Button
+            type='button'
+            variant='outline'
+            size='sm'
+            className='mt-2 ml-8'
+            onClick={() => {
+              router.push(`/forms/preview/${formId}`);
+            }}
+          >
+            Preview
+          </Button>
+          <Button
+            type='button'
+            size='sm'
+            className='mt-2 ml-2'
+            onClick={async () => {
+              await togglePublishFormFromUser(formId);
+            }}
+          >
+            {form.published ? 'Unpublish' : 'Publish'}
+          </Button>
+          {form.published ? (
+            <div>
+              <Button
+                type='button'
+                size='sm'
+                className='mt-8'
+                onClick={async () => {
+                  await navigator.clipboard.writeText(
+                    `${window.location.origin}/forms/viewform/${formId}`
+                  );
+                  toast({
+                    title: 'Link successfully copied',
+                  });
+                }}
+              >
+                Copy Link
+              </Button>
+              <Button
+                type='button'
+                size='sm'
+                variant='outline'
+                className='mt-8 ml-2'
+                onClick={async () => {
+                  router.push(`/forms/viewform/${formId}`);
+                }}
+              >
+                Go to form
+              </Button>
+            </div>
+          ) : null}
+        </div>
+        <div className='mt-12'>
+          {questions.map((element: any) => {
+            return (
+              <div key={element.id} className='mb-5 group relative'>
+                <Input
+                  defaultValue={element.text}
+                  key={element.id + '2'}
+                  placeholder='Type a question'
+                  className='w-1/2 border-0 shadow-none focus-visible:ring-0 pl-0 !mt-0 !pt-0 scroll-m-20 tracking-tight transition-colors leading-7 [&:not(:first-child)]:mt-0'
+                  onChange={(e) => debounced(element.id, null, e.target.value)}
+                />
+                <Input
+                  defaultValue={element.placeholder}
+                  placeholder='Type a placeholder for the response'
+                  key={element.id + '1'}
+                  className=' w-1/2 leading-7 [&:not(:first-child)]:mt-0 text-muted-foreground'
+                  onChange={(e) => debounced(element.id, e.target.value, null)}
+                />
+                <div className='absolute top-2 left-0 transform-translate-x-full hidden group-hover:inline-flex'>
+                  <div className='mr-6'>
+                    <div className='px-2 hover:cursor-pointer'>
+                      <Plus
+                        className='text-gray-700'
+                        onClick={async () => {
+                          await createQuestion(formId, element.order + 1);
+                        }}
+                      />
+                    </div>
+                    <div
+                      className='px-2 mt-1 hover:cursor-pointer'
                       onClick={async () => {
-                        await createQuestion(formId, element.order + 1);
+                        await deleteQuestion(formId, element.id);
                       }}
-                    />
-                  </div>
-                  <div
-                    className='px-2 mt-1 hover:cursor-pointer'
-                    onClick={async () => {
-                      await deleteQuestion(formId, element.id);
-                    }}
-                  >
-                    <Trash2 className='mt-1 text-gray-700' />
+                    >
+                      <Trash2 className='mt-1 text-gray-700' />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
