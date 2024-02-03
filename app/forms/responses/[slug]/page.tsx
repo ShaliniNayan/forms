08f1e402ff7e3827d95d7d296d7789311b021f81
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-03 20:00:53 +0530

    Add pie charts to responsive

--- app/forms/responses/[slug]/page.tsx
+++ app/forms/responses/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ResponsePie from '@/components/pie';
 import {
   Card,
   CardContent,
@@ -6,30 +7,56 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { getResponsesSummaryFromUser } from '@/lib/actions';
+import { MoveLeft } from 'lucide-react';
 import Link from 'next/link';
 
 function Question({ question }: any) {
-  return (
-    <Card className='col-span-3 mt-8'>
-      <CardHeader>
-        <CardTitle>{question.text}</CardTitle>
-        <CardDescription>{`${question.answers.length} responses`}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className='space-y-8'>
-          {question.answers.map((answer: any) => {
-            return (
-              <div key={answer.key} className='ml-4 space-y-1'>
-                <p className='text-sm font-medium text-muted-foreground'>
-                  {answer.text}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-      </CardContent>
-    </Card>
-  );
+  if (question.type === 'SHORT_RESPONSE') {
+    return (
+      <Card className='col-span-3 mt-8'>
+        <CardHeader>
+          <CardTitle>{question.text}</CardTitle>
+          <CardDescription>{`${question.answers.length} responses`}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='space-y-8'>
+            {question.answers.map((answer: any) => {
+              return (
+                <div key={answer.key} className='ml-4 space-y-1'>
+                  <p className='text-sm font-medium text-muted-background'>
+                    {answer.answerText}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </CardContent>
+      </Card>
+    );
+  } else if (question.type === 'MANY_OPTIONS') {
+    return (
+      <Card className='col-span-3 mt-8'>
+        <CardHeader>
+          <CardTitle>{question.text}</CardTitle>
+          <CardDescription>{`${question.answers.length} responses`}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='space-y-8'>
+            <ResponsePie />
+            {question.answers.map((answer: any) => {
+              return (
+                <div key={answer.key} className='ml-4 space-y-1'>
+                  <p className='text-sm font-medium text-muted-background'>
+                    {answer.answerText}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
@@ -38,7 +65,20 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <div className='mx-48 my-20'>
       <div className='my-10'>
-        <Link href={`/forms`}>{'<-- Back to forms'}</Link>
+        <Link href={`/forms`}>
+          <div className='flex items-center'>
+            {
+              <MoveLeft
+                className='mr-2'
+                color='#000000'
+                strokeWidth={1.75}
+                absoluteStrokeWidth
+                size={18}
+              />
+            }
+            {'Back to my forms'}
+          </div>
+        </Link>
       </div>
       <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'>
         Responses
