0c17a7248745fab8351cbe75fb8aead6e873055a
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-03 20:01:38 +0530

    Update option view form

---
+++ app/forms/viewform/[slug]/form.tsx
@@ -0,0 +1,127 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export default function Form({
+  questions,
+  submitForm,
+  form,
+}: {
+  questions: any;
+  submitForm: any;
+  form: any;
+}) {
+  const route = useRouter();
+  const [answers, setAnswers] = useState(
+    questions.reduce((acc: any, question: any) => {
+      if (question.type === 'SHORT_RESPONSE') {
+        acc[question.id] = {
+          type: 'SHORT_RESPONSE',
+          optionId: null,
+          text: null,
+        };
+      } else if (question.type === 'MANY_OPTIONS') {
+        acc[question.id] = {
+          type: 'MANY_OPTIONS',
+          optionId: null,
+          text: null,
+        };
+      }
+
+      return acc;
+    }, {})
+  );
+
+  return (
+    <div>
+      <div className='mt-12'>
+        {questions.map((element: any) => {
+          if (element.type === 'SHORT_RESPONSE') {
+            return (
+              <div key={element.id} className='mb-5 group relative'>
+                <div className='sm:w-1/2 tracking-tight flex h-9 w-full rounded-md border-0 bg-transparent py-1 text-sm transition-colors leading-7 file:border-0 file:bg-transparent file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'>
+                  {element.text}
+                </div>
+                <Input
+                  onChange={(e) => {
+                    const newValue = e.target.value;
+                    setAnswers((prevAnswers: any) => ({
+                      ...prevAnswers,
+                      [element.id]: {
+                        ...prevAnswers[element.id],
+                        text: newValue,
+                      },
+                    }));
+                  }}
+                  placeholder={element.placeholder ? element.placeholder : ''}
+                  key={element.id + '1'}
+                  className='sm:w-1/2 leading-7 [&:not(:first-child)]:mt-0 text-muted-foreground'
+                />
+              </div>
+            );
+          } else if ('MANY_OPTIONS') {
+            return (
+              <div key={element.id} className='mb-5 group relative'>
+                <div className='sm:w-1/2 tracking-tight flex h-9 w-full rounded-md border-0 bg-transparent py-1 text-sm transition-colors leading-7 file:border-0 file:bg-transparent file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'>
+                  {element.text}
+                </div>
+                <QuestionRadioGroup
+                  setAnswers={setAnswers}
+                  options={element.options}
+                  questionId={element.id}
+                />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className='mt-8'>
+        <Button
+          onClick={async () => {
+            await submitForm(answers, form.id);
+            route.push(`/forms/success/${form.id}`);
+          }}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+const QuestionRadioGroup = ({ options, setAnswers, questionId }: any) => {
+  return (
+    <RadioGroup
+      onValueChange={(value) => {
+        const newValue = value;
+        setAnswers((prevAnswers: any) => ({
+          ...prevAnswers,
+          [questionId]: {
+            ...prevAnswers[questionId],
+            optionId: newValue,
+          },
+        }));
+      }}
+    >
+      {options.map((option: any) => {
+        return (
+          <div
+            key={option.id}
+            className='flex items-center space-x-2 relative group'
+          >
+            <RadioGroupItem value={option.id} id={option.id} />
+            <Input
+              defaultValue={option.optionText}
+              placeholder='type the option here'
+              className='w-1/2 border-0 shadow-none  focus-visible:ring-0 pl-0 !mt-0 !pt-0 scroll-m-20 tracking-tight transition-colors leading-7 [&:not(:first-child)]:mt-0'
+            />
+          </div>
+        );
+      })}
+    </RadioGroup>
+  );
+};
