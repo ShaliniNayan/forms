a397c5f47ec12ce847dbb71f96e75ff1e607cd74
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-05 19:51:50 +0530

    Update functionality of pie chart from recharts

--- components/pie.tsx
+++ components/pie.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Cell, Legend } from 'recharts';
 
 const data = [
   { name: 'Group A', value: 400 },
@@ -12,6 +12,27 @@ const COLORS = ['#0088FE', '#00c49f', '#FFBB28', '#FF8042'];
 
 const RADIAN = Math.PI / 180;
 
+const renderLegend = (props: any) => {
+  const { payload } = props;
+  const totalValue = payload.reduce((acc: any, entry: any) =>
+    acc + Number.isFinite(entry.value) ? entry.value : 0
+  );
+  0;
+  return (
+    <ul>
+      {payload.map((entry: any, index: any) => {
+        const percentage =
+          totalValue !== 0 ? ((entry.value / totalValue) * 100).toFixed(2) : 0;
+        return (
+          <li key={`item-${index}`} style={{ color: entry.color }}>
+            {entry.name} ({percentage}%)
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
 const renderCustomizedLabel = ({
   cx,
   cy,
@@ -21,6 +42,10 @@ const renderCustomizedLabel = ({
   percent,
   index,
 }: any) => {
+  const MIN_PERCENT_FOR_LABEL = 0.2;
+
+  if (percent < MIN_PERCENT_FOR_LABEL) return null;
+
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
@@ -38,9 +63,21 @@ const renderCustomizedLabel = ({
   );
 };
 
-const ResponsePie = () => {
+type PieData = {
+  name: string;
+  value: string;
+}[];
+
+type ResponsePieProps = {
+  data: PieData;
+};
+
+const ResponsePie = ({ data }: ResponsePieProps) => {
+  if (!data || data.length === 0) {
+    return null;
+  }
   return (
-    <ResponsiveContainer width='100%' height={200}>
+    <ResponsiveContainer width='50%' height={200}>
       <PieChart width={400} height={200}>
         <Pie
           data={data}
@@ -51,11 +88,22 @@ const ResponsePie = () => {
           outerRadius={80}
           fill='#8884d8'
           dataKey='value'
+          isAnimationActive={false}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
+        <Legend
+          height={24}
+          verticalAlign='top'
+          layout='vertical'
+          wrapperStyle={{
+            paddingRight: '0px',
+            paddingLeft: '0px',
+            fontSize: '12px',
+          }}
+        />
       </PieChart>
     </ResponsiveContainer>
   );
