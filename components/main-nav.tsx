3e71229b715ca4689abb773193375ce59f1cafdd
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-09 20:26:35 +0530

    Add main page nav bar

---
+++ components/main-nav.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Link from 'next/link';
+import { LoginLink } from './loginLink';
+import { RegisterLink } from './registerLink';
+import Logout from './logout';
+import { Button } from './ui/button';
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+
+export function MainNav({ isUserLogged }: { isUserLogged: boolean }) {
+  return (
+    <div className='mr-4 hidden md:block'>
+      <div className='border-b'>
+        <div className='flex h-16 items-centre px-32'>
+          <h4 className='font-bold tracking-tight cursor-pointer'>
+            SimpleForms
+          </h4>
+          <div className='ml-8 text-sm font-medium transition-colors hover:text-foreground/80 text-foreground/60 cursor-pointer'>
+            <Link href={''}>Documentation</Link>
+          </div>
+          <div className='ml-auto flex items-center space-x-6 text-sm font-medium'>
+            {!isUserLogged && <LoginLink />}
+            {!isUserLogged && <RegisterLink />}
+            {isUserLogged && <Logout />}
+            <div>
+              <Button variant='outline'>
+                <GitHubLogoIcon className='h-4 w-4' />
+                <span className='ml-2 text-xs'>STAR US</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
