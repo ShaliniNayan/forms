4cf079459d7df14d0458b9542ecf4536c2a54954
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-15 20:55:20 +0530

    Fix the bug

--- lib/exportToExcel.ts
+++ lib/exportToExcel.ts
@@ -7,7 +7,7 @@ export function exportToExcel(
 ): void {
   const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(processedData);
   const wb: XLSX.WorkBook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  // XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
   const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
 
