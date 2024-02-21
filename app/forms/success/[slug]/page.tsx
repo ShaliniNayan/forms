7595c5dfc34833e108eb9aca09dd71b14594429e
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-21 17:35:37 +0530

    Update the response page after submit

--- app/forms/success/[slug]/page.tsx
+++ app/forms/success/[slug]/page.tsx
@@ -1,9 +1,19 @@
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { getForm } from '@/lib/actions/actions';
 import { RocketIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
-export default function Page() {
+export default async function Page({ params }: { params: { slug: string } }) {
+  const formId = params.slug;
+  const form = await getForm(formId);
+
+  const title = form.title;
   return (
-    <div className='my-28 mx-28'>
+    <div className='mx-4 mt-12 md:mx-32 xl:mx-96 xl:mt-24'>
+      <h2 className='mb-10 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors'>
+        {title}
+      </h2>
       <Alert>
         <RocketIcon className='w-4 h-4' />
         <AlertTitle>Submission Successful</AlertTitle>
@@ -11,6 +21,13 @@ export default function Page() {
           Your form has successfully been submitted. Thank you for your time!
         </AlertDescription>
       </Alert>
+      <div className='mt-4 md:mt-8'>
+        <Link href={`/forms/viewform/${formId}`}>
+          <Button className='px-0' variant='link'>
+            {'Send another response'}
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
