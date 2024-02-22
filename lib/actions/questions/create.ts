0660c7d2988b080b28ed7f3b228fbfb98b9d6102
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-02-23 00:43:19 +0530

    Update create option

--- lib/actions/questions/create.ts
+++ lib/actions/questions/create.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
-import { getSession } from '@/lib/auth';
+import { getSession } from '@/lib//auth';
 import { prisma } from '@/lib/prisma';
 import { redirect } from 'next/navigation';
 
@@ -49,9 +49,9 @@ export const createOptionQuestion = async (
       userId: session.user.id,
       formId,
       order: questionOrder,
-      type: 'MANY_OPTIONS',
+      type: 'SELECT_ONE_OPTION',
       options: {
-        create: [{ order: 1, optionText: 'option 1' }],
+        create: [{ order: 1, optionText: 'Option 1' }],
       },
     },
   });
@@ -60,7 +60,7 @@ export const createOptionQuestion = async (
 
   await prisma.$transaction(updateOperations);
 
-  revalidatePath(`/forms/${formId}`);
+  revalidatePath(`forms/${formId}`);
 
   return;
 };
@@ -76,20 +76,21 @@ export const createShortResponseQuestion = async (
     };
   }
 
-  const fromFromUser = await prisma.form.findFirst({
+  const formFromUser = await prisma.form.findFirst({
     where: {
       id: formId,
     },
   });
-  if (!fromFromUser) {
+
+  if (!formFromUser) {
     return {
-      error: 'Form does not exit',
+      error: 'Form does not exist',
     };
   }
 
-  if (fromFromUser.userId !== session.user.id) {
+  if (formFromUser.userId !== session.user.id) {
     return {
-      error: 'Form is not not from the user',
+      error: 'Form is not form user',
     };
   }
 
@@ -108,13 +109,8 @@ export const createShortResponseQuestion = async (
   const updateOperations = questions.map((question) => {
     const newOrder = question.order + 1;
     return prisma.question.update({
-      where: {
-        id: question.id,
-        formId,
-      },
-      data: {
-        order: newOrder,
-      },
+      where: { id: question.id, formId },
+      data: { order: newOrder },
     });
   });
 
@@ -131,7 +127,66 @@ export const createShortResponseQuestion = async (
 
   await prisma.$transaction(updateOperations);
 
-  revalidatePath(`/forms/${formId}`);
+  revalidatePath(`forms/${formId}`);
+
+  return;
+};
+
+export const createMultipleOptionQuestion = async ({
+  formId,
+  questionOrder,
+}: {
+  formId: string;
+  questionOrder: number;
+}) => {
+  const session = await getSession();
+  if (!session?.user.id) {
+    return {
+      error: 'Not authenticated',
+    };
+  }
+
+  const form = await prisma.form.findFirstOrThrow({
+    where: {
+      id: formId,
+      userId: session.user.id,
+    },
+    include: {
+      questions: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+  });
+
+  const { questions } = form;
+
+  const updateOperations = questions.map((question) => {
+    const newOrder = question.order + 1;
+    return prisma.question.update({
+      where: { id: question.id, formId },
+      data: { order: newOrder },
+    });
+  });
+
+  const createQuestionFunction = prisma.question.create({
+    data: {
+      userId: session.user.id,
+      formId,
+      order: questionOrder,
+      type: 'SELECT_MULTIPLE_OPTIONS',
+      options: {
+        create: [{ order: 1, optionText: 'Option 1' }],
+      },
+    },
+  });
+
+  updateOperations.push(createQuestionFunction);
+
+  await prisma.$transaction(updateOperations);
+
+  revalidatePath(`forms/${formId}`);
 
   return;
 };
